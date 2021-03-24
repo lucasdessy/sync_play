1d4ffddeeed3a4f399cf677b976ff88bdd167890
lucasdessy <lucasedu49@gmail.com>
2021-03-24 14:41:35 -0300

    made app service

---
+++ lib/services/app_service.dart
@@ -0,0 +1,5 @@
+// Might convert to an GetxService later
+class AppService {
+  static const defaultProfilePic =
+      'https://firebasestorage.googleapis.com/v0/b/sync-play-46e93.appspot.com/o/default_assets%2FProfile-PNG-Icon.png?alt=media&token=d2f7dcaf-6709-4ea9-84a4-cf806c4edbc7';
+}
