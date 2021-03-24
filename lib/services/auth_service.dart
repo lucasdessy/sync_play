00e29df843d71691101c758052657514219ccf03
lucasdessy <lucasedu49@gmail.com>
2021-03-24 10:50:11 -0300

    improve login() and recoverPassword() methods

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:sync_play/models/app_error.dart';
 
 class AuthService extends GetxService {
   final _auth = FirebaseAuth.instance;
@@ -25,6 +26,29 @@ class AuthService extends GetxService {
   }
 
   Future<void> login({required String email, required String password}) async {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: email.trim(), password: password);
+    } on FirebaseAuthException catch (e) {
+      String? details;
+      if (e.code == 'user-not-found') {
+        details = 'Usuário não existe';
+      } else if (e.code == 'wrong-password') {
+        details = 'Senha está errada';
+      }
+      throw AppError('Erro ao fazer login', errorDetails: details);
+    } catch (e) {
+      print(e);
+      throw AppError('Ocorreu um erro interno');
+    }
+  }
+
+  Future<void> recoverPassword({required String email}) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email.trim());
+    } catch (e) {
+      print(e);
+      throw AppError('Ocorreu um erro interno');
+    }
   }
 }
