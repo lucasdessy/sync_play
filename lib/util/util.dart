cfe411a420cf4ccb0bcca3c96e39d5313145d85c
lucasdessy <lucasedu49@gmail.com>
2021-03-24 10:50:37 -0300

    added util docs and showSnacbar()

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sync_play/models/app_error.dart';
 
 class Util {
-  static void showError(AppError e) {
-    Get.snackbar(e.errorDetails == null ? 'Ocorreu um erro' : e.errorTitle,
-        e.errorDetails ?? e.errorTitle);
+  /// Shows a snackbar. All parameters required.
+  static void showSnackbar(BuildContext context,
+      {required String title, required String details}) {
+    Get.snackbar(title, details,
+        colorText: CupertinoTheme.of(context).textTheme.textStyle.color);
+  }
+
+  /// Shows a snackbar with error details.
+  /// Works only with [AppError] class.
+  static void showError(AppError e, BuildContext context) {
+    showSnackbar(context,
+        title: e.errorDetails == null ? 'Ocorreu um erro' : e.errorTitle,
+        details: e.errorDetails ?? e.errorTitle);
   }
 }
