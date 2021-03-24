17578506df5bee7ea7e92fa6500923fceb8f1740
lucasdessy <lucasedu49@gmail.com>
2021-03-24 16:08:02 -0300

    added additional null checks to profile tab controller

--- lib/ui/pages/home/tabs/profile/profile_tab_controller.dart
+++ lib/ui/pages/home/tabs/profile/profile_tab_controller.dart
@@ -1,10 +1,17 @@
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:sync_play/services/app_service.dart';
 import 'package:sync_play/services/auth_service.dart';
+import 'package:sync_play/services/route_service.dart';
 
 class ProfileTabController extends GetxController {
   final _authService = Get.find<AuthService>();
-  String? get userName => _authService.currentUser()!.name;
-  String? get userEmail => _authService.authUser()!.email;
-  String get userPic => _authService.currentUser()!.profilePicUrl;
+  String? get userName => _authService.currentUser()?.name;
+  String? get userEmail => _authService.authUser()?.email;
+  String get userPic =>
+      _authService.currentUser()?.profilePicUrl ?? AppService.defaultProfilePic;
+
+  void handleGoToEdit() {
+    Get.toNamed(RouteService.EDIT_PROFILE);
+  }
 }
