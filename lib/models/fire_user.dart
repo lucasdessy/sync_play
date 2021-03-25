33e55253e0c7a28a45a9dd74c7f595f85f21d2cf
lucasdessy <lucasedu49@gmail.com>
2021-03-25 11:09:13 -0300

    added token to fireUser

--- lib/models/fire_user.dart
+++ lib/models/fire_user.dart
@@ -5,18 +5,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sync_play/services/app_service.dart';
 
 class FireUser {
+  String? id;
   String? name;
   late String profilePicUrl;
+  String? token;
 
   FireUser({this.name, this.profilePicUrl = AppService.defaultProfilePic});
 
   Map<String, dynamic> toJson() => <String, dynamic>{
         'name': name,
         'profilePicUrl': profilePicUrl,
+        'token': token,
       };
 
   FireUser.fromDocument(DocumentSnapshot snapshot) {
     name = snapshot.data()?['name'];
     profilePicUrl = snapshot.data()?['profilePicUrl'];
+    token = snapshot.data()?['token'];
   }
 }
